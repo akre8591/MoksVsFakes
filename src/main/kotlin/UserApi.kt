532c2a6410985a268e8fb596e6686e8b96f50beb
Diego Casas <diego.casas@wgu.edu>
2023-04-28 23:57:33 -0600

    Add space

--- src/main/kotlin/UserApi.kt
+++ src/main/kotlin/UserApi.kt
@@ -1,22 +1,22 @@
 class UserApi {
 
     private val listOfUsers = listOf(
-        User(1230, "Diego", "Casas",32, Gender.Male),
-        User(1231, "Karen", "Wright",25, null),
-        User(1232, "Lynda", "Smith",20, Gender.Female),
-        User(1233, "Bradley", "Johnson",44, Gender.Male),
-        User(1234, "Adam", "Williams",34, Gender.Male),
-        User(1235, "Paul", "Brown",58, Gender.Male),
-        User(1236, "Sophia", "Jones",28, Gender.Female),
-        User(1237, "Aaron", "Garcia",23, Gender.Male),
-        User(1238, "Emma", "Miller",32, Gender.Female),
-        User(1239, "Walter", "White",null, Gender.Male),
-        User(1240, "Drake", "Davis",45, Gender.Male),
-        User(1241, "Marshall", "Anderson",38, Gender.Male),
-        User(1242, "Selena", "Lewis",27, Gender.Female),
-        User(1243, "Laurence", "Thomas",22, Gender.Female),
-        User(1244, "Olivia", "Wilson",42, Gender.Female),
-        User(1245, "Bob", "Clark",30, Gender.Male),
+        User(1230, "Diego", "Casas", 32, Gender.Male),
+        User(1231, "Karen", "Wright", 25, null),
+        User(1232, "Lynda", "Smith", 20, Gender.Female),
+        User(1233, "Bradley", "Johnson", 44, Gender.Male),
+        User(1234, "Adam", "Williams", 34, Gender.Male),
+        User(1235, "Paul", "Brown", 58, Gender.Male),
+        User(1236, "Sophia", "Jones", 28, Gender.Female),
+        User(1237, "Aaron", "Garcia", 23, Gender.Male),
+        User(1238, "Emma", "Miller", 32, Gender.Female),
+        User(1239, "Walter", "White", null, Gender.Male),
+        User(1240, "Drake", "Davis", 45, Gender.Male),
+        User(1241, "Marshall", "Anderson", 38, Gender.Male),
+        User(1242, "Selena", "Lewis", 27, Gender.Female),
+        User(1243, "Laurence", "Thomas", 22, Gender.Female),
+        User(1244, "Olivia", "Wilson", 42, Gender.Female),
+        User(1245, "Bob", "Clark", 30, Gender.Male),
     )
 
     fun fetchUserById(userId: Int): User? {
